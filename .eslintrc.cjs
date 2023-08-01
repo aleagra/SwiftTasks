c3d2ed9acd8d34b3dbe5352906e083d0924a85fa
Alejandro Agra <aleagra08@hotmail.com>
2023-08-01 20:37:19 -0300

    vite, react, typescript, eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    "react/react-in-jsx-scope":"off"
+  },
+};
